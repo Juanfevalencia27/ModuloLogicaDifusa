faf1eb83dc1928f2b2e47e42cad37b84b34fc76b
Juanfevalencia27 <48332577+Juanfevalencia27@users.noreply.github.com>
2020-02-04 20:57:33 -0500

    Add files via upload

---
+++ Nutricion LD/src/Main.java
@@ -0,0 +1,34 @@
+
+import java.io.IOException;
+import net.sourceforge.jFuzzyLogic.FIS;
+import net.sourceforge.jFuzzyLogic.plot.JFuzzyChart;
+import net.sourceforge.jFuzzyLogic.rule.Rule;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan Felipe
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        String fileName = "src/Archivo FCL.fcl";
+        FIS fis = FIS.load(fileName, true);
+        // Error while loading?
+        if (fis == null) {
+            System.err.println("Can't load file: '" + fileName + "'");
+            return;
+        }
+
+        MainView view = new MainView(fis);
+        view.setVisible(true);
+
+    }
+}
